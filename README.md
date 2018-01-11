a9c45474ff117c9f5282be235dbd995e35ae7371
Becca Ricks <baricks@gmail.com>
2018-01-11 17:32:27 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# facebook-ad-cleaner-script #
+
+Periodically clears out your ad preferences & categories on Facebook by programmatically going through [this webpage](https://www.facebook.com/ads/preferences/) and removing your interests & ads you've interacted with.
+
+TODO:
+* Set this up as a cronjob so it runs on a regular routine (once a night?)
+* Spin the script into a full-fledged browser extension
---
+++ script.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+# Set up Driver
+driver = webdriver.PhantomJS()
+driver.get("http://www.facebook.com/login")
+
+# Log into Facebook
+login = driver.find_element_by_css_selector('#email')
+login.send_keys('YOUR-USERNAME-HERE')
+password = driver.find_element_by_css_selector('#pass')
+password.send_keys('YOUR-PASSWORD-HERE')
+password.send_keys(Keys.RETURN)
+time.sleep(2)
+
+# Go to ad preferences
+url = "https://www.facebook.com/ads/preferences/"
+driver.get(url)
+time.sleep(2)
+
+# Click each like to remove
+
+likes = driver.find_elements_by_css_selector('button[title]')
+time.sleep(2)
+for like in likes:
+    like.click()
+    time.sleep(2)
+    print ("clicked")
+
+driver.quit()
